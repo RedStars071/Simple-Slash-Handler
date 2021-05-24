ff3cdf26d137059581398aec5bd5024222eee28e
Red171js <autistadeimostri171@gmail.com>
2021-05-24 12:53:49 +0200

    update 4

---
+++ ints/ping.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: 'ping',
+    description: 'Ping!',
+    commandOptions: null,
+    global: true,
+    run: async(interaction, client) => {
+        
+        client.api.interactions(interaction.id, interaction.token).callback.post({data: {
+            type: 4,
+            data: {
+                embeds: [
+                  {
+                    title: 'Pong:',
+                    description: `:ping_pong: ${client.ws.ping}ms!`
+                    
+                }
+                ]
+            }
+        
+        }
+
+        })
+    }
+};
